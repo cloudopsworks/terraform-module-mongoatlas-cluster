78a0fc4eff8504351c7f7de259a07e6b75b9c83c
Cristian Beraha <cristian@cloudops.works>
2024-12-03 11:29:50 -0300

    chore: addition of creation of default Database Admin User

--- admin-user.tf
+++ admin-user.tf
@@ -7,7 +7,7 @@
 resource "mongodbatlas_database_user" "admin_user" {
   count              = try(var.settings.admin_user.enabled, false) ? 1 : 0
   auth_database_name = try(var.settings.admin_user.auth_database, "admin")
-  project_id         = data.mongodbatlas_project.this.id
+  project_id         = var.project_id != "" ? var.project_id : data.mongodbatlas_project.this[0].id
   username           = try(var.settings.admin_user.username, "${local.system_name_short}-admin-user")
   password           = random_password.randompass[count.index].result
 
