d0472d4791f997339271acf3ab534475ca6ece97
Cristian Beraha <cristian@cloudops.works>
2024-12-06 22:16:31 -0300

    chore: connection strings revisited better handling in some situations

--- admin-user.tf
+++ admin-user.tf
@@ -8,7 +8,7 @@ resource "mongodbatlas_database_user" "admin_user" {
   count              = try(var.settings.admin_user.enabled, false) ? 1 : 0
   auth_database_name = try(var.settings.admin_user.auth_database, "admin")
   project_id         = var.project_id != "" ? var.project_id : data.mongodbatlas_project.this[0].id
-  username           = try(var.settings.admin_user.username, "${var.name != "" ? var.name : format("%s-%s", var.name_prefix, local.system_name_short)}-admin-user")
+  username           = try(var.settings.admin_user.username, "${var.name != "" ? lower(var.name) : format("%s-%s", var.name_prefix, local.system_name_short)}-admin-user")
   password           = random_password.randompass[count.index].result
 
   roles {
